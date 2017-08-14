f44424ef8f3450f2278a0b1259a9a5a31c2c7d94
Elliot Jackson <elliotekj@gmail.com>
2017-08-14 15:12:20 +0200

    Run link tests on URL field too
    — Elliot Jackson <elliot@elliotekj.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate regex;
 
 mod spam_phrases;
 
-use regex::Regex;
+use regex::{Regex, Captures};
 
 #[derive(Debug)]
 pub enum Status {
@@ -29,7 +29,8 @@ pub struct Snooker {
 
 lazy_static! {
     // Matches links, capturing the value in their `href`:
-    static ref LINK_RE: Regex = Regex::new(r#"<a[^>]*href=["']((https?://)?([\da-zA-Z.-]+)\.([a-zA-Z]{2,10})[/]?([?]?[\S]*))["'][^>]*>"#).unwrap();
+    static ref A_TAG_RE: Regex = Regex::new(r#"<a[^>]*href=["']((https?://)?([\da-zA-Z.-]+)\.([a-zA-Z]{2,10})[/]?([?]?[\S]*))["'][^>]*>"#).unwrap();
+    static ref URL_RE: Regex = Regex::new(r#"((https?://)?([\da-zA-Z.-]+)\.([a-zA-Z]{2,10})[/]?([?]?[\S]*))"#).unwrap();
 
     // Matches 5 or more consonants in a row:
     static ref CONSONANTS_RE: Regex = Regex::new(r#"(?i)[b-z&&[^eiou]]{5,}"#).unwrap();
@@ -51,40 +52,15 @@ impl Snooker {
         }
     }
 
-    pub fn process_links(&mut self) -> i8 {
+    pub fn check_body_links(&mut self) -> i8 {
         let mut link_count = 0;
+        let body_clone = self.comment.body.clone();
 
-        for c in LINK_RE.captures_iter(&self.comment.body) {
+        for c in A_TAG_RE.captures_iter(&body_clone) {
             // Count the number of links
             link_count += 1;
 
-            // Check for certain TLDs
-
-            let tld = &c[4];
-
-            for naughty_tld in NAUGHTY_TLDS.iter() {
-                if &tld == naughty_tld {
-                    self.score -= 1;
-                }
-            }
-
-            // Check for certains words & characters
-
-            let url = &c[1];
-
-            for word in URL_SPAM_WORDS.iter() {
-                if url.to_lowercase().contains(word) {
-                    self.score -= 1;
-                }
-            }
-
-            // Check the length of the URL:
-            if url.len() > 30 {
-                self.score -= 1;
-            }
-
-            // Check for 5 consonants or more in a row:
-            self.score -= count_consonants(url) as i8;
+            process_single_link(c, self);
         }
 
         if link_count <= 2 {
@@ -94,6 +70,16 @@ impl Snooker {
         }
 
         link_count
+    }
+
+    pub fn check_url(&mut self) {
+        let url_option = self.comment.clone().url;
+
+        if let Some(url) = url_option {
+            if let Some(c) = URL_RE.captures(&url) {
+                process_single_link(c, self);
+            };
+        };
     }
 
     pub fn check_body_length(&mut self, link_count: i8) {
@@ -140,10 +126,11 @@ impl Snooker {
 pub fn process_comment(comment: Comment) -> Snooker {
     let mut snooker = Snooker::new(comment);
 
-    let link_count = snooker.process_links();
+    let link_count = snooker.check_body_links();
     snooker.check_body_length(link_count);
     snooker.check_body_for_spam_phrases();
     snooker.check_body_first_word();
+    snooker.check_url();
     snooker.check_author_for_http();
 
     println!("{}", snooker.score);
@@ -163,6 +150,38 @@ pub fn count_consonants(s: &str) -> u8 {
     count
 }
 
+fn process_single_link(c: Captures, snooker: &mut Snooker) {
+    // Check for certain TLDs
+
+    let tld = &c[4];
+
+    for naughty_tld in NAUGHTY_TLDS.iter() {
+        if &tld == naughty_tld {
+            snooker.score -= 1;
+
+            break;
+        }
+    }
+
+    // Check for certains words & characters
+
+    let url = &c[1];
+
+    for word in URL_SPAM_WORDS.iter() {
+        if url.to_lowercase().contains(word) {
+            snooker.score -= 1;
+        }
+    }
+
+    // Check the length of the URL:
+    if url.len() > 30 {
+        snooker.score -= 1;
+    }
+
+    // Check for 5 consonants or more in a row:
+    let consonant_count = count_consonant_collections(url) as i8;
+
+    snooker.score -= consonant_count;
 #[cfg(test)]
 mod tests {
     use super::*;
