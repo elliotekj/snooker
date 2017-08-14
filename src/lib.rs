ab495b29a88dbad3c91c76a01cde49ec2c8b679a
Elliot Jackson <elliotekj@gmail.com>
2017-08-14 09:03:05 +0200

    Deduct points for certain words in the URL
    — Elliot Jackson <elliot@elliotekj.com>

--- src/lib.rs
+++ src/lib.rs
@@ -27,10 +27,11 @@ pub struct Snooker {
 
 lazy_static! {
     // Matches links, capturing the value in their `href`:
-    static ref LINK_RE: Regex = Regex::new(r#"<a[^>]*href=["'](https?://)?([\da-zA-Z.-]+)\.([a-z.]{2,6})["'][^>]*>"#).unwrap();
+    static ref LINK_RE: Regex = Regex::new(r#"<a[^>]*href=["']((https?://)?([\da-zA-Z.-]+)\.([a-zA-Z]{2,10})[/]?([?]?[\S]*))["'][^>]*>"#).unwrap();
 }
 
 static NAUGHTY_TLDS: [&str; 3] = ["de", "pl", "cn"];
+static URL_SPAM_WORDS: [&str; 5] = [".html", ".info", "?", "&", "free"];
 
 impl Snooker {
     pub fn new(comment: Comment) -> Self {
@@ -45,15 +46,27 @@ impl Snooker {
         let mut link_count = 0;
 
         for c in LINK_RE.captures_iter(&self.comment.body) {
+            println!("{:?}", c);
+
             // Count the number of links
             link_count += 1;
 
             // Check for certain TLDs
 
-            let tld = &c[3];
+            let tld = &c[4];
 
             for naughty_tld in NAUGHTY_TLDS.iter() {
                 if &tld == naughty_tld {
+                    self.score -= 1;
+                }
+            }
+
+            // Check for certains words & characters
+
+            let url = &c[1];
+
+            for word in URL_SPAM_WORDS.iter() {
+                if url.contains(word) {
                     self.score -= 1;
                 }
             }
@@ -87,9 +100,9 @@ mod tests {
             author: None,
             email: None,
             url: None,
-            body: String::from("<p>This <a href=\"https://elliotekj.com\">comment</a> \
+            body: String::from("<p>This <a href=\"https://elliotekj-free.com\">comment</a> \
             has more <a href=\"https://elliotekj.de\">than</a> 20 characters in \
-            it but <a href=\"https://elliotekj.com\">contains</a> 3 links.</p>"),
+            it but <a href=\"https://elliotekj.com?some=param\">contains</a> 3 links.</p>"),
         };
 
         process_comment(comment);
