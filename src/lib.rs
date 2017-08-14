9f0c238d46383520e7bbae5019ebd2e3ba5400c1
Elliot Jackson <elliotekj@gmail.com>
2017-08-14 08:12:49 +0200

    Setup basic structures
    — Elliot Jackson <elliot@elliotekj.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+pub enum Status {
+    Valid,
+    Moderate,
+    Spam,
+}
+
+#[derive(Debug)]
+pub struct Comment {
+    pub author: Option<String>,
+    pub email: Option<String>,
+    pub url: Option<String>,
+    pub body: String,
+}
+
+#[derive(Debug)]
+pub struct Snooker {
+    pub score: i8,
+    pub status: Status,
+    pub comment: Comment,
+}
+impl Snooker {
+    pub fn new(comment: Comment) -> Self {
+        Snooker {
+            score: 0,
+            status: Status::Moderate,
+            comment: comment,
+        }
+    }
+
+    pub fn process_links(&mut self) {
+    }
+}
+
+pub fn process_comment(comment: Comment) -> Snooker {
+    let mut snooker = Snooker::new(comment);
+
+    snooker.process_links();
+
+    snooker
+}
 #[cfg(test)]
 mod tests {
     #[test]
