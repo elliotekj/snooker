7f4e7c20f92f5d43cba52e685c7cfbc7067e9c54
Elliot Jackson <elliotekj@gmail.com>
2017-08-14 08:13:18 +0200

    Count the number of links in the body
    — Elliot Jackson <elliot@elliotekj.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+use regex::{Regex, Captures};
+
 #[derive(Debug)]
 pub enum Status {
     Valid,
@@ -19,6 +24,12 @@ pub struct Snooker {
     pub status: Status,
     pub comment: Comment,
 }
+
+lazy_static! {
+    // Matches links, capturing the value in their `href`:
+    static ref LINK_RE: Regex = Regex::new(r#"<a[^>]*href=["']([^\s"'>]+)["'][^>]*>"#).unwrap();
+}
+
 impl Snooker {
     pub fn new(comment: Comment) -> Self {
         Snooker {
@@ -29,6 +40,17 @@ impl Snooker {
     }
 
     pub fn process_links(&mut self) {
+        let mut link_count = 0;
+
+        for c in LINK_RE.captures_iter(&self.comment.body) {
+            link_count += 1;
+        }
+
+        if link_count <= 2 {
+            self.score += 2;
+        } else {
+            self.score -= link_count;
+        }
     }
 }
 
@@ -39,9 +61,22 @@ pub fn process_comment(comment: Comment) -> Snooker {
 
     snooker
 }
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
+        let comment = Comment {
+            author: None,
+            email: None,
+            url: None,
+            body: String::from("<p>This <a href=\"https://elliotekj.com\">comment</a> \
+            has more <a href=\"https://elliotekj.com\">than</a> 20 characters in \
+            it but <a href=\"https://elliotekj.com\">contains</a> 3 links.</p>"),
+        };
+
+        process_comment(comment);
     }
 }
